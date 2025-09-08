ebe06d60fa457874a1fdf9c35c795127fb4e3cdb
Karthik G K <kar221694@gmail.com>
2025-09-08 23:42:52 +0530

    Update char_drivers.c

--- char_drivers.c
+++ char_drivers.c
@@ -13,27 +13,11 @@
 
 // Device numbers : kernel assigns one number to identify the device number containg 2 parts
 // major number : minor number (actual device driver identifier and device instance)
-
-// Device Driver is a piece of code that configure and manages a device
-// In Kernel space we can see different types of device drivers
-// CHAR drivers, Block device drivers and network device drivers
-// Kernel will interact with these drivers to perform I/O operations
-
-// CHAR drivers : access data from the device sequentially that is byte by byte
-// no advanced buffering startegy is required as data is going byte by byte
-// eg : keyboard
-
-//Block drivers : access data from the device in fixed-size blocks
-// They are responsible for managing the read and write requests for these blocks
-// Block drivers typically use a buffer cache to improve performance eg ssd
-
-// Device numbers : kernel assigns one number to identify the device number containg 2 parts
-// major number : minor number (actual device driver identifier and device instance)
 // nencessary for the kernel to identify and manage the device driver
 #include <linux/module.h>
 #include <linux/fs.h>
 
-dev_t dev;
+dev_t dev_number; // device number variable
 struct cdev cdev;
 /*
 struct cdev {
@@ -61,13 +45,15 @@ struct file_operations {
 
 static int __init driver_init(void){
     /*Assign a device number*/
-    alloc_chrdev_region(&dev, 0, 1, "my_device");
-    printk(KERN_INFO "Device number assigned: Major: %d, Minor: %d\n", MAJOR(dev), MINOR(dev));
+    alloc_chrdev_region(&dev_number, 0, 1, "my_device");
+    printk(KERN_INFO "Device number assigned: Major: %d, Minor: %d\n", MAJOR(dev_number), MINOR(dev_number));
 
     /* Initialize cdev structure with fops */
-    cdev.owner = THIS_MODULE; // THIS_MODULE is a macro that points to the current module
     cdev_init(&cdev, &fops);
-    
+	cdev.owner = THIS_MODULE; // THIS_MODULE is a macro that points to the current module
+
+    /* Add cdev to kernel VFS*/
+	cdev_add(&cdev, dev, 1);
 
     return 0;
 }
