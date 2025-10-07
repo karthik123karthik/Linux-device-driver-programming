7c06ec39c33b213b95cb6161a7b3e4827c1307be
Karthik G K <kar221694@gmail.com>
2025-10-07 20:56:19 +0530

    Update platform_driver.c

--- platform_driver.c
+++ platform_driver.c
@@ -3,6 +3,20 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 
+struct pcdev_private_data {
+    struct platform_data *pdata;
+    char* buffer;
+    dev_t dev_num;
+    struct cdev cdev;
+};
+
+struct driver_private_data {
+    int total_devices;
+    dev_t device_num_base;
+    struct class *class_pcd;
+    struct device *device_pcd;
+};
+
 // called when platform device is matched with driver
 int probe(struct platform_device *pdev) {
     printk(KERN_INFO "Platform device probed: %s\n", pdev->name);
@@ -23,13 +37,40 @@ struct platform_driver my_platform_driver = {
     }};
 
 static __init int platform_device_driver_init(void) {
+    int ret;
+
+    ret = alloc_char_region(&driver_data.device_num_base, 0, 2, "pcd_devices");
+    if (ret < 0) {
+        printk(KERN_ALERT "Alloc char region failed\n");
+        return ret;
+    }
+    // create class
+    driver_data.class_pcd = class_create(THIS_MODULE, "pcd_class");
+    if (IS_ERR(driver_data.class_pcd)) {
+        unregister_chrdev_region(driver_data.device_num_base, 2);
+        printk(KERN_ALERT "Class creation failed\n");
+        return PTR_ERR(driver_data.class_pcd);
+    }
+    // create device
+    driver_data.device_pcd = device_create(driver_data.class_pcd, NULL, driver_data.device_num_base, NULL, "pcd_device");
+    if (IS_ERR(driver_data.device_pcd)) {
+        class_destroy(driver_data.class_pcd);
+        unregister_chrdev_region(driver_data.device_num_base, 2);
+        printk(KERN_ALERT "Device creation failed\n");
+        return PTR_ERR(driver_data.device_pcd);
+    }
+    // register platform driver
     platform_driver_register(&my_platform_driver);
     printk(KERN_INFO "Platform device and driver example initialized\n");
     return 0;
 }
 
 static __exit void platform_device_driver_exit(void) {
+    //platform device unregistration is handled in platform_device_setup.c
     platform_driver_unregister(&my_platform_driver);
+    device_destroy(driver_data.class_pcd, driver_data.device_num_base);
+    class_destroy(driver_data.class_pcd);
+    unregister_chrdev_region(driver_data.device_num_base, 2);
     printk(KERN_INFO "Platform device and driver example exited\n");
 }
 
